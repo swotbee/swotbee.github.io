ae961fb839f3025c77f47614abac8fe823e5d39f
reputebee <website@reputebee.com>
2025-07-13 15:49:42 +0530

    carosel implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import alpine from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
-    tailwind()
+    tailwind(),
+    alpine()
   ],
   output: 'static'
 });
