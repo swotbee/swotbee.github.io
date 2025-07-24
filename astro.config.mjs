b2b1a48b5dc0371357361dcaa8d80c85bfde00f9
reputebee <website@reputebee.com>
2025-07-24 13:12:53 +0530

    ci: switch to Astro build and GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,17 @@ import alpine from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. Set your Pages URL
+  site: 'https://swotbee.github.io',
+
+  // 2. Set base to your repo name (include leading & trailing slash)
+  base: '/swotbee.github.io/',
+
+  // 3. Output static files
+  output: 'static',
   integrations: [
     react(),
     tailwind(),
     alpine()
-  ],
-  output: 'static'
+  ]
 });
