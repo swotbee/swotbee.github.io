750d786772981402d5203b4c07c1af627c7c043b
reputebee <website@reputebee.com>
2025-07-24 21:55:31 +0530

    feat: enhance security headers and improve testimonial image handling in BlogPostLayout

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,26 @@ export default defineConfig({
   site: "https://swotbee.github.io",
   output: "static",
   integrations: [react(), alpine(), tailwind()],
+  server: {
+    headers: {
+      "Content-Security-Policy": [
+  //       "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;",
+  //       "script-src * 'unsafe-inline' 'unsafe-eval' data: blob:;",
+  //       "style-src * 'unsafe-inline';",
+  //       "img-src * data: blob:;",
+  //       "font-src * data:;",
+  //       "connect-src * ws: wss:;",
+  //       "media-src * data: blob:;",
+  //       "object-src 'none';",
+  //       "base-uri 'self';",
+  //       "form-action 'self';",
+  //       "frame-ancestors 'none';",
+        // "upgrade-insecure-requests;"
+      ].join(" "),
+      "X-Content-Type-Options": "nosniff",
+      "X-Frame-Options": "DENY",
+      "X-XSS-Protection": "1; mode=block",
+      // "Referrer-Policy": "strict-origin-when-cross-origin"
+    }
+  }
 });
